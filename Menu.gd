7b8698645fe9bb8da6696d7fafa771660fb96310
Victor Ssuto <83074897+VictorSsuto@users.noreply.github.com>
2024-01-20 15:24:44 -0500

    Play rules exit saved
    fdsfsad

--- Menu.gd
+++ Menu.gd
@@ -1,4 +1,16 @@
 extends Control 
-
+#Play button
 func _on_play_pressed():
 	pass # Replace with function body.
+	
+
+
+#Rules button
+func _on_rules_pressed():
+	pass # Replace with function body.
+
+
+#Exit button 
+func _on_quit_pressed():
+
+	get_tree().quit
