2908b241e959d927694c91985a0e6313fba2d126
RiadMefti <riadmefti@hotmail.com>
2024-01-20 18:07:59 -0500

    movement change

--- scenes/movement.gd
+++ scenes/movement.gd
@@ -1,70 +1,46 @@
 extends CharacterBody2D
 
-@export var speed: int = 100
-var current_dir = "none"
+@export var speed = 400
 
-var currentHealth: int = 3
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
 
 func _ready():
 	$AnimatedSprite2D.play("front_idle")
 
-func _physics_process(delta):
-	player_movement(delta)
 
-func player_movement(delta):
-	
-	if Input.is_action_pressed("move_right"):
-		current_dir="right"
-		play_anim(1)
-		velocity.x = speed
-		velocity.y = 0
-	elif Input.is_action_pressed("move_left"):
-		current_dir="left"
-		play_anim(1)
-		velocity.x = -speed
-		velocity.y = 0
-	elif Input.is_action_pressed("move_up"):
-		current_dir="up"
-		play_anim(1)
-		velocity.x = 0
-		velocity.y = -speed
-	elif Input.is_action_pressed("move_down"):
-		current_dir="down"
-		play_anim(1)
-		velocity.x = 0
-		velocity.y = speed
-	else:
-		play_anim(0)
-		velocity.x = 0
-		velocity.y = 0
 
-	move_and_slide()
-	
-func play_anim(movement):
-	var dir = current_dir
-	var anim = $AnimatedSprite2D
-	
-	if dir == "right":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("side_walk")
-		elif movement == 0:
-			anim.play("side_idle")
-	if dir == "left":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("side_walk")
-		elif movement == 0:
-			anim.play("side_idle")
-	if dir == "down":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("front_walk")
-		elif movement == 0:
-			anim.play("front_idle")
-	if dir == "up":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("back_walk")
-		elif movement == 0:
-			anim.play("back_idle")
+	#
+#func play_anim(movement):
+	#var dir = current_dir
+	#var anim = $AnimatedSprite2D
+	#
+	#if dir == "right":
+		#anim.flip_h = false
+		#if movement == 1:
+			#anim.play("side_walk")
+		#elif movement == 0:
+			#anim.play("side_idle")
+	#if dir == "left":
+		#anim.flip_h = true
+		#if movement == 1:
+			#anim.play("side_walk")
+		#elif movement == 0:
+			#anim.play("side_idle")
+	#if dir == "down":
+		#anim.flip_h = true
+		#if movement == 1:
+			#anim.play("front_walk")
+		#elif movement == 0:
+			#anim.play("front_idle")
+	#if dir == "up":
+		#anim.flip_h = true
+		#if movement == 1:
+			#anim.play("back_walk")
+		#elif movement == 0:
+			#anim.play("back_idle")
