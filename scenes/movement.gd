ca55190f93526489fdd8bfdea32dbd1dd971b377
nyannnnn <90939473+nyannnnn@users.noreply.github.com>
2024-01-20 16:54:29 -0500

    implemented animation

--- scenes/movement.gd
+++ scenes/movement.gd
@@ -11,19 +11,39 @@ func _physics_process(delta):
 func player_movement(delta):
 	
 	if Input.is_action_pressed("move_right"):
+		current_dir="right"
+		play_anim(1)
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("move_left"):
+		current_dir="left"
+		play_anim(1)
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("move_up"):
+		current_dir="up"
+		play_anim(1)
 		velocity.x = 0
 		velocity.y = -speed
 	elif Input.is_action_pressed("move_down"):
+		current_dir="down"
+		play_anim(1)
 		velocity.x = 0
 		velocity.y = speed
 	else:
+		play_anim(0)
 		velocity.x = 0
 		velocity.y = 0
 
 	move_and_slide()
+	
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("side_walk")
+		elif movement == 0:
+			anim.play("side_idle")
