b48a1ac5620b2d11bef37ff3097d3106625fe5f9
nyannnnn <90939473+nyannnnn@users.noreply.github.com>
2024-01-21 02:48:07 -0500

    death animation + laser

--- scenes/laser.gd
+++ scenes/laser.gd
@@ -8,3 +8,7 @@ func _process(delta):
 	position += speed * delta
 	if position.x > get_viewport_rect().size.x:
 		queue_free()  # Delete the laser when it goes off-screen
+
+
+func _on_area_2d_area_entered(area):
+	self.queue_free()
