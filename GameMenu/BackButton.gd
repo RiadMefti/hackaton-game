e7f84bc2c5c0521e03ec1c77580c3d70508451d7
Victor Ssuto <83074897+VictorSsuto@users.noreply.github.com>
2024-01-21 04:15:39 -0500

    Added sounds and fix loops

--- GameMenu/BackButton.gd
+++ GameMenu/BackButton.gd
@@ -2,5 +2,6 @@ extends Button
 
 
 
+
 func _on_pressed():
 	get_tree().change_scene_to_file("res://GameMenu/menu.tscn")
--- scenes/Death.gd
+++ scenes/Death.gd
@@ -1,6 +1,14 @@
 extends Control
 
 
+
+
 func _on_main_menu_pressed():
-		#get_tree().change_scene_to_file("res://GameMenu/menu.tscn")
-	pass
+	get_tree().change_scene_to_file("res://GameMenu/menu.tscn")
+
+
+
+
+
+func _on_button_pressed():
+	get_tree().quit()
